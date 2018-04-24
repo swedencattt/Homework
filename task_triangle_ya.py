ef3e09f8b73002af4f5a2750a8c469fa21566b53
swedencattt <swedencattt@gmail.com>
2018-04-24 23:40:59 +0300

    homework 01

--- task_triangle_ya.py
+++ task_triangle_ya.py
@@ -1,5 +1,15 @@
-x = int(input())
-y = int(inp  ut())
-z = input()
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+x3 = int(input())
+y3 = int(input())
 
-d = 
+ab = (x1 - x2) ** 2 + (y1 - y2) ** 2
+ac = (x1 - x3) ** 2 + (y1 - y3) ** 2
+bc = (x2 - x3) ** 2 + (y2 - y3) ** 2
+
+if ab == bc + ac or bc == ab + ac or ac == ab + bc:
+	print('yes')
+else:
+	print('no')
